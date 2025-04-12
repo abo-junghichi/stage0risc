0cc9dee5830bbfb2c473e3275504cacfaa841137
Junghichi Abo <abo_junghichi@yahoo.co.jp>
2025-04-13 06:05:39 +0900

    stack-isa: rename c-code.out for bootstrap

--- stack-isa/assemble-boot.sh
+++ stack-isa/assemble-boot.sh
@@ -6,8 +6,8 @@ cat ../autolabel.img head.asm - | $VM 2>log >tmp.lm
 if [ -s log ] ; then exit 1 ; fi
 cat ../linemacro.img tmp.lm | $VM 2>log >tmp.nl
 if [ -s log ] ; then exit 1 ; fi
-if ./numlabel.c.out <tmp.nl >tmp.img ; then true ; else exit 1 ; fi
-./genheader.c.out <tmp.img >bh.img
+if ./numlabel.out <tmp.nl >tmp.img ; then true ; else exit 1 ; fi
+./genheader.out <tmp.img >bh.img
 cat bh.img tmp.img
 
 rm tmp.lm tmp.nl log tmp.img bh.img
--- stack-isa/bootstrap.sh
+++ stack-isa/bootstrap.sh
@@ -2,11 +2,11 @@
 
 ( cd ../ ; make emu-fast.out )
 CC="gcc -O3 -Wall -pedantic -Wextra -Wstrict-aliasing=1"
-$CC genheader.c -o genheader.c.out
-$CC numlabel.c -o numlabel.c.out
+$CC genheader.c -o genheader.out
+$CC numlabel.c -o numlabel.out
 for stem in bootloader genheader numlabel linemacro autolabel
 do
 	./assemble-boot.sh < $stem.asm > $stem.img
 done
 $CC emu.c -o emu.out
-rm genheader.c.out numlabel.c.out
+rm genheader.out numlabel.out
